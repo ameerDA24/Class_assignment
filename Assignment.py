215d0258be81926f8d01bb06da9e3fcba8bd81c7
ameerDA24 <118303230+ameerDA24@users.noreply.github.com>
2022-11-30 10:58:57 +0100

    Update Assignment.py

--- Assignment.py
+++ Assignment.py
@@ -27,42 +27,41 @@ a=int(input())
 b=int(input())
 rem=a%b
 print("The remainder when a is divided by b is", rem)
-
 #1.6
 import math
 a=int(input())
 print(math.log10(a))
 
-#1.7
 a=int(input())
 b=int(input())
 square=a**b
 print("The squere is", square)
 
-#2
+#2.1
 mpg=float(input())
 liters_per_hund_km= 235.21/mpg
 print(mpg, "miles-per-gallon are equivalent", liters_per_hund_km, "L/100km")
 
-#3
+
+
+
+#3.1
 import math                  #degrees = radian * (180° / pi)
-                             #a float value, representing the value in degrees
+                            #a float value, representing the value in degrees
 lat1=float(input())         
 long1=float(input())
 lat2=float(input())
 long2=float(input())
 
-
-lat1_r= math.degrees(lat1)   #converting into radians because distance f-cion (with sin, cos etc) uses radians as inputs
+  #converting into radians because distance f-cion (with sin, cos etc) uses radians as inputs
+lat1_r= math.degrees(lat1) 
 long1_r=math.degrees(long1)
 lat2_r=math.degrees(lat2)
 long2_r=math.radians(long2)
-
 distance=6371.01*math.acos((math.sin(lat1_r)*math.sin(lat2_r)+math.cos(lat1_r)*math.cos(lat2_r)*math.cos(long1_r-long2_r)))
-
 print(distance)
 
-#4
+#4.1
 distance_feet = int(input("distance in feet: "))
 distance_inches = distance_feet * 12
 distance_yards = distance_feet / 3.0
@@ -72,23 +71,27 @@ print("The distance in inches is %i inches." % distance_inches)
 print("The distance in yards is %.2f yards." % distance_yards)
 print("The distance in miles is %.2f miles." % distance_miles)
 
-#5
+
+
+
+
+#5.1
 import math
 
 n=int(input("the number of sides: "))
 s=float(input("the length of a side: "))
-
 area= n*(s**2)/4*math.tan(math.pi/n)
-
 print("Area of a regular polygon is", area)
 
-#6
+#6.1
 from datetime import datetime
 
 current_datatime= datetime.now()
 print(current_datatime)
 
-#7
+
+
+#7.1
 Ta=float(input("temperature in degrees C is "))
 V=float(input("wind speed in km/h id "))
 
@@ -96,12 +99,3 @@ WCI= round(13.12+0.6215*Ta-11.37* V**0.16 +0.3965* Ta* V**0.16, 2)
 
 print("The wind chill index is ", WCI)
 
-#8
-string=input("enter a four digits")
-
-my_list=[int(x) for x in str(string)]
-
-sum=0
-for i in my_list:
-    sum+=i
-print("The sum of digits are",sum)
